a98dbbaee81ef3c6d1a1faf45aa8cc254e15894d
ryan woods <federer5050@gmail.com>
2020-12-01 17:38:07 +0000

    adding instance to class

--- app.js
+++ app.js
@@ -15,4 +15,48 @@ class UI {
       this.itemList = [];
       this.itemID = 0;
     }
-  }
+//   submit budget method
+submitBudgetForm(){
+    const value = this.budgetInput.value;
+    if(value==='' || value < 0) {
+        this.budgetFeedback.classList.add('showItem');
+        this.budgetFeedback.innerHTML = `<p>value cannot be empty or
+        negative</p>`;
+        // 'this' is the global window object... now we can access budgetFeedback in setTimeout function
+        const self = this;
+        console.log(this);
+        
+        setTimeout(function(){
+            self.budgetFeedback.classList.remove('showItem');
+        }, 4000 );
+        }
+    }
+}
+
+  function eventListeners(){
+      const budgetForm = document.getElementById('budget-form');
+      const expenseForm = document.getElementById('expense-form');
+      const expenseList = document.getElementById('expense-list');
+
+//   new instance of UI Class
+const ui = new UI();
+
+// budget form submit
+budgetForm.addEventListener('submit', function(){
+    event.preventDefault();
+    ui.submitBudgetForm();
+});
+
+// expense form submit
+expenseForm.addEventListener('submit', function(event){
+    event.preventDefault();
+});
+
+    // expense click
+expenseList.addEventListener('submit', function(event){});
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    eventListeners();
+});
+
