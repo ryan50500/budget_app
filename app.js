65615339a42c6a94335f21e5c5b4ab6648cfc386
ryan woods <federer5050@gmail.com>
2020-12-03 15:07:24 +0000

    expense function calculation added

--- app.js
+++ app.js
@@ -54,6 +54,61 @@ submitBudgetForm(){
             this.balance.classList.add('showBlack');
         }
     }
+    // submit expense form
+    submitExpenseForm() {
+        const expenseValue = this.expenseInput.value;
+        const amountValue = this.amountInput.value;
+    if(expenseValue === '' || amountValue === '' || amountValue < 0 ) 
+    {
+        this.expenseFeedback.classList.add('showItem');
+        this.expenseFeedback.innerHTML = `<p>values cannot be empty or 
+        negative</p>`;
+        const self = this;
+        setTimeout(function(){
+            self.expenseFeedback.classList.remove('showItem');
+            }, 4000);
+        } else {
+            let amount = parseInt(amountValue);
+            this.expenseInput.value = "";
+            this.amountInput.value = "";
+
+            let expense = {
+                id:this.itemID,
+                title:expenseValue,
+                amount:amount,
+            }
+            // each item added has unique ID
+            this.itemID++;
+            // adding expense object we created above to 'itemList' array 
+            this.itemList.push(expense);
+            this.addExpense(expense);
+            // show balance
+        }
+    }
+    // add expense
+    addExpense(expense){
+        const div = document.createElement('div');
+        div.classList.add('expense');
+        div.innerHTML = `
+                <div class="expense-item d-flex justify-content-between align-items-baseline">
+                <h6 class="expense-title mb-0 text-uppercase list-item">- 
+                ${expense.title}</h6>
+                <h5 class="expense-amount mb-0 list-item">${expense.amount}
+                </h5>
+
+                <div class="expense-icons list-item">
+                <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+                <i class="fas fa-edit"></i>
+                </a>
+                <a href="#" class="delete-icon" data-id="${expense.id}">
+                <i class="fas fa-trash"></i>
+                </a>
+                </div>
+            </div>
+       `;
+       this.expenseList.appendChild(div);
+    }
+
     // total expense
     totalExpense(){
         let total = 400;
@@ -78,6 +133,7 @@ budgetForm.addEventListener('submit', function(){
 // expense form submit
 expenseForm.addEventListener('submit', function(event){
     event.preventDefault();
+    ui.submitExpenseForm();
 });
 
     // expense click
