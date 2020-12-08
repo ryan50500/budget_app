c2c6d169dcb3c39e3c0c058b76459e9d089502ea
ryan woods <federer5050@gmail.com>
2020-12-08 13:48:03 +0000

    expense calculations

--- app.js
+++ app.js
@@ -125,11 +125,36 @@ submitBudgetForm(){
     }
     // edit expense
     editExpense(element){
-        let id = parseInt()
+        let id = parseInt(element.dataset.id)
+        let parent = element.parentElement.parentElement.parentElement;
+        // remove from dom
+        this.expenseList.removeChild(parent);
+        // remove from the list
+        let expense = this.itemList.filter(function(item){
+            return item.id === id;
+        });
+        // show value
+        this.expenseInput.value = expense[0].title;
+        this.amountInput.value = expense[0].amount;
+        // remove from list
+        let tempList = this.itemList.filter(function(item){
+            return item.id !==id;
+        });
+        this.itemList = tempList;
+        this.showBalance();
     }
       // delete expense
       deleteExpense(element){
-
+        let id = parseInt(element.dataset.id)
+        let parent = element.parentElement.parentElement.parentElement;
+        // remove from dom
+        this.expenseList.removeChild(parent);
+         // remove from list
+         let tempList = this.itemList.filter(function(item){
+            return item.id !==id;
+        });
+        this.itemList = tempList;
+        this.showBalance();
     }
 }
 
@@ -156,6 +181,7 @@ expenseForm.addEventListener('submit', function(event){
     // expense click
 expenseList.addEventListener('submit', function(event){
     if(event.target.parentElement.classList.contains('edit-icon')){
+        consolge.
 ui.editExpense(event.target.parentElement)
     }
     else if(event.target.parentElement.classList.contains
